9f0614cbc46b45edb8871ca8a322ad5a2e08706e
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2024-06-17 18:56:20 -0300

    fix(126): Instala o Helmet

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   const config = new DocumentBuilder()
     .setTitle('RMS API')
     .setDescription('[ Base URL: http://localhost:3000/ ]')
